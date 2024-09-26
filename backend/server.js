43586b67f6984755962bf8629effab6ee7ece007
S.V.HARIHARAN <106609383+HariharanAI@users.noreply.github.com>
2024-09-26 13:15:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,22 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const port = 3004;
-const todoRoutes = require("./routes/todos"); // Update this line
-
-const mongoURI = "mongodb://username:password@mongo:27017/todos";
+const routes = require("./routes");
 
 main().catch((err) => console.log(err));
 
 async function main() {
-    try {
-        await mongoose.connect(mongoURI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        });
-        console.log("Connected to MongoDB");
+  await mongoose.connect("mongodb://mongo:27017/todos", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use("/api", routes);
 
-        const app = express();
-        app.use(cors());
-        app.use(express.json());
-        app.use("/api", todoRoutes); // Use the todos route here
-
-        app.listen(port, () => {
-            console.log(`Server is listening on port: ${port}`);
-        });
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-    }
+  app.listen(port, () => {
+    console.log(`Server is listening on port: ${port}`);
+  });
 }
