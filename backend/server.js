9af667efbf6f19ab2bd9671f7fadeeeaa1800a6e
S.V.HARIHARAN <106609383+HariharanAI@users.noreply.github.com>
2024-09-26 12:43:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,29 @@ const cors = require('cors');
 const mongoose = require("mongoose");
 const port = 3001;
 const routes = require("./routes");
-const API_URL = 'http://Backend:3001';
+
+// Replace 'username' and 'password' with your MongoDB credentials
+const mongoURI = "mongodb://username:password@mongo:27017/todos";
 
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://mongo:27017/todos", {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
-  const app = express();
-  app.use(cors());
-  app.use(express.json());
-  app.use("/api", routes);
+    try {
+        await mongoose.connect(mongoURI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
+        console.log("Connected to MongoDB");
 
-  app.listen(port, () => {
-    console.log(`Server is listening on port: ${port}`);
-  });
+        const app = express();
+        app.use(cors());
+        app.use(express.json());
+        app.use("/api", routes);
+
+        app.listen(port, () => {
+            console.log(`Server is listening on port: ${port}`);
+        });
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
 }
